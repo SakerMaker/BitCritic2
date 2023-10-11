9977a1d0a3511923fbf9a802643d5298f2753701
Saker <paguyale@gmail.com>
2023-10-11 09:28:37 +0200

    Edit Profile

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,12 +1,6 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div>
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="px-4 py-5 px-md-5 text-center text-lg-start d-flex align-items-center justify-content-center bg-dark" style="background-color: hsl(0, 0%, 96%);min-height:78vh;">
+        <div class="container">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
 
@@ -42,4 +36,5 @@
             @endif
         </div>
     </div>
+
 </x-app-layout>
