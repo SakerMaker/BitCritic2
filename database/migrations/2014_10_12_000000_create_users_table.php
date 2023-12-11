3ba6152c5dbd8988c95be88f85fae655db0df6c8
Saker <paguyale@gmail.com>
2023-12-11 15:29:31 +0100

    Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 4096)->default('img/BitCritic-Profile-Picture-Default-View-Game-Review-Community.png')->nullable();
-            $table->string('banner_photo_path', 16384)->default('img/BitCritic-Banner-Default-View-Game-Review-Community.png')->nullable();
+            $table->string('banner_photo_path', 16383)->default('img/BitCritic-Banner-Default-View-Game-Review-Community.png')->nullable();
             $table->timestamps();
         });
     }
