b649c252898b047401d864131f2f599fe3ff91d0
Saker <36892961+SakerMaker@users.noreply.github.com>
2023-12-14 14:08:08 +0100

    Update mail template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'BitCritic')
-<img src="bitcritic.es/img/BitCritic-Logo-View-Game-Review-Community.png" class="logo" alt="BitCritic Logo">
+<img src="https://bitcritic.es/img/BitCritic-Logo-View-Game-Review-Community.png" class="logo" alt="BitCritic Logo">
 @else
 {{ $slot }}
 @endif
