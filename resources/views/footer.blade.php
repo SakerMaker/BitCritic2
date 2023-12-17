6e2b29e04cbc65c8d6b1ec1490b74b7cfb92710a
Saker <36892961+SakerMaker@users.noreply.github.com>
2023-12-17 19:25:31 +0100

    Update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,6 +4,8 @@
             <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; BitCritic 2023</div></div>
             <div class="col-auto sm-text-center">
                 {{-- <a class="link-light small" href="https://forms.gle/nEm4ERmr5WGV5uFG8" target="_blank">Recomendar Juego</a> --}}
+                <a class="link-light small" href="{{ url("/Ayuda_de_la_Aplicación_BitCritic_17-12-2023.pdf") }}" target="_blank">Ayuda de la Aplicación</a>
+                <span class="text-white mx-1">&middot;</span>
                 <a class="link-light small" href="{{ Route('policy.show') }}" target="_blank">Política de Privacidad</a>
                 <span class="text-white mx-1">&middot;</span>
                 <a class="link-light small" href="{{ Route('terms.show') }}" target="_blank">Términos y Condiciones</a>
