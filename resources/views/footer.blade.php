d1388e61b38de2ce655e73a3fff8992014aff4c3
Saker <36892961+SakerMaker@users.noreply.github.com>
2023-12-17 19:27:39 +0100

    Small update view

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container px-5">
         <div class="d-flex align-items-center justify-content-between flex-column flex-md-row text-lg-start text-center">
             <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; BitCritic 2023</div></div>
-            <div class="col-auto sm-text-center">
+            <div class="d-flex flex-wrap justify-content-md-end justify-content-center">
                 {{-- <a class="link-light small" href="https://forms.gle/nEm4ERmr5WGV5uFG8" target="_blank">Recomendar Juego</a> --}}
                 <a class="link-light small" href="{{ url("/Ayuda_de_la_Aplicación_BitCritic_17-12-2023.pdf") }}" target="_blank">Ayuda de la Aplicación</a>
                 <span class="text-white mx-1">&middot;</span>
